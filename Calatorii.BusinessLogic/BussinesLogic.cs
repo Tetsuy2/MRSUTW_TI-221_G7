2c5c1d5bd1f33c879cea42e7da25211628d64c3e
Mihai <144902209+Tetsuy2@users.noreply.github.com>
2024-05-29 21:58:28 +0300

    Business Logic

---
+++ Calatorii.BusinessLogic/BussinesLogic.cs
@@ -0,0 +1,11 @@
+﻿using Calatorii.BusinessLogic.Interfaces;
+namespace Calatorii.BusinessLogic
+{
+     public class BussinesLogic
+     {
+          public ISession GetSessionBL()
+          {
+               return new SessionBL();
+          }
+     }
+}
---
+++ Calatorii.BusinessLogic/SessionBL.cs
@@ -0,0 +1,19 @@
+﻿using Calatorii.BusinessLogic.Core;
+using Calatorii.BusinessLogic.Interfaces;
+using Calatorii.Domain.Entities.User;
+using Calatorii.Domain.Entities.User.Responses;
+namespace Calatorii.BusinessLogic
+{
+     public class SessionBL : UserApi, ISession
+     {
+          public new ULoginResp UserLoginAction(ULoginData _login)
+          {
+               return base.UserLoginAction(_login);
+          }
+        public new URegisterResp UserRegisterAction(URegisterData _register) 
+        {
+            return base.UserRegisterAction(_register);
+        }
+
+     }
+}
