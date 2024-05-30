d3a4a9fce28e24123ee915823cebc4059d2d89b4
Mihai <144902209+Tetsuy2@users.noreply.github.com>
2024-05-30 09:46:32 +0300

    Business logic

---
+++ Calatorii.Web/Controllers/RegisterController.cs
@@ -0,0 +1,55 @@
+﻿using Calatorii.BusinessLogic;
+using Calatorii.BusinessLogic.Interfaces;
+using Calatorii.Domain.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Calatorii.Web.Models;
+
+
+namespace Calatorii.Web.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly ISession _session;
+        public RegisterController()
+        {
+            var bl = new BussinesLogic();
+            _session = bl.GetSessionBL();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(UserRegister register)
+        {
+            if (ModelState.IsValid)
+            {
+                URegisterData data = new URegisterData
+                {
+                    Credential = register.Credential,
+                    Password = register.Password,
+                    Email = register.Email,
+                    RegisterIp = Request.UserHostAddress,
+                    RegisterDateTime = DateTime.Now
+                };
+
+               var userRegister = _session.UserRegisterAction(data);
+                if (userRegister.Status)
+                {
+                     return RedirectToAction("Register", "Home");
+                }
+                else
+                {
+                     ModelState.AddModelError("", userRegister.StatusMsg);
+                     return View();
+                }
+            }
+            return RedirectToAction("Register", "Home");
+        }
+    }
+}
---
+++ Calatorii.Web/Models/Review.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calatorii.Web.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
